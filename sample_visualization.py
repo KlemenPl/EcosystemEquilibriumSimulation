8771a02b9e43dfe1300dec9e8beb6266c6036969
Klemen Plestenjak <plestenjak.klemen@gmail.com>
2025-01-15 23:55:26 +0100

    fix: Visualize saved simulation

--- sample_visualization.py
+++ sample_visualization.py
@@ -3,39 +3,10 @@ from ecosystem_simulation.simulation_player import *
 
 
 def main():
+    #opts: SimulationOptions = SimulationOptions.from_json_file("optimization_results/best_20250115-214026_481.json")
+    opts: SimulationOptions = SimulationOptions.from_json_file("optimization_results/best_2.json")
     simulator = EcosystemSimulator(
-        options_=SimulationOptions(
-            randomness_seed=77779113,
-            world_width=64,
-            world_height=64,
-            max_vision_distance=16,
-            child_gene_mutation_chance_when_mating= 0.1,
-            child_gene_mutation_magnitude_when_mating=0.05,
-            food_item_spawning_rate_per_tick=5,
-            food_item_life_tick=80,
-            initial_number_of_food_items=200,
-            max_number_of_food_items=400,
-            predator=EntitySimulationOptions(
-                initial_number=20,
-                initial_satiation_on_spawn=0.3,
-                max_juvenile_in_ticks=30,
-                max_gestation_in_ticks=20,
-                max_age_in_ticks=300,
-                max_children_per_birth=3,
-                satiation_per_feeding=0.8,
-                satiation_loss_per_tick=0.025,
-            ),
-            prey=EntitySimulationOptions(
-                initial_number=120,
-                initial_satiation_on_spawn=0.2,
-                max_juvenile_in_ticks=30,
-                max_gestation_in_ticks=20,
-                max_age_in_ticks=300,
-                max_children_per_birth=5,
-                satiation_per_feeding=0.6,
-                satiation_loss_per_tick=0.005,
-            )
-        )
+        options_=opts,
     )
 
     player = SimulationPlayer(mode=PlayerMode.SIMULATOR, source=simulator)
